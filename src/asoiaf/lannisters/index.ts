3c5537e22cf78976f69bc026c2c49d4529296640
Brian Chuchua <bchuchua@gmail.com>
2022-09-20 21:10:51 -0700

    feat: add baratheon banners
    Closes #8

--- src/asoiaf/lannisters/index.ts
+++ src/asoiaf/lannisters/index.ts
@@ -1,7 +1,8 @@
 import Attachments from './attachments';
 import CombatUnits from './combatUnits';
+import InformationCards from './informationCards';
 import NonCombatUnits from './nonCombatUnits';
 import OpponentAttachments from './opponentAttachments';
 import TacticsCards from './tacticsCards';
 
-export default { ...CombatUnits, ...Attachments, ...NonCombatUnits, ...OpponentAttachments, ...TacticsCards };
+export default { ...CombatUnits, ...Attachments, ...NonCombatUnits, ...OpponentAttachments, ...TacticsCards, ...InformationCards };
---
+++ src/asoiaf/lannisters/informationCards.ts
@@ -0,0 +1,12 @@
+import { CardData, CardTypes } from '../types';
+
+const cardData: CardData = {
+  'Baratheon Banners': {
+    name: 'Baratheon Banners',
+    type: CardTypes.InformationCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hl-baratheon-banners.png',
+    imageUrlBack: '',
+  },
+};
+
+export default cardData;
