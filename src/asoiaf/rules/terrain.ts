80ba7b62e340e887a65819ee3e19ffac41a7b604
Brian Chuchua <bchuchua@gmail.com>
2023-03-04 21:47:34 -0800

    feat: add terrain

--- src/asoiaf/rules/terrain.ts
+++ src/asoiaf/rules/terrain.ts
@@ -7,6 +7,60 @@ const cardData: CardData = {
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/terrain-castle-walls.png',
     imageUrlBack: '',
   },
+  Terrain: {
+    name: 'Terrain',
+    type: CardTypes.Terrain,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/terrain.png',
+    imageUrlBack: '',
+  },
+  Bog: {
+    name: 'Bog',
+    type: CardTypes.Terrain,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/terrain-bog.png',
+    imageUrlBack: '',
+  },
+  CorpsePile: {
+    name: 'Corpse Pile',
+    type: CardTypes.Terrain,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/terrain-corpse-pile.png',
+    imageUrlBack: '',
+  },
+  Forest: {
+    name: 'Forest',
+    type: CardTypes.Terrain,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/terrain-forest.png',
+    imageUrlBack: '',
+  },
+  Hedge: {
+    name: 'Hedge',
+    type: CardTypes.Terrain,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/terrain-hedge.png',
+    imageUrlBack: '',
+  },
+  LowWallRuinedWall: {
+    name: 'Low Wall / Ruined Wall',
+    type: CardTypes.Terrain,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/terrain-low-wall-ruined-wall.png',
+    imageUrlBack: '',
+  },
+  Palisade: {
+    name: 'Palisade',
+    type: CardTypes.Terrain,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/terrain-palisade.png',
+    imageUrlBack: '',
+  },
+  Stakes: {
+    name: 'Stakes',
+    type: CardTypes.Terrain,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/terrain-stakes.png',
+    imageUrlBack: '',
+  },
+  WeirwoodTree: {
+    name: 'Weirwood Tree',
+    type: CardTypes.Terrain,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/terrain-weirwood-tree.png',
+    imageUrlBack: '',
+  },
 };
 
 export default cardData;
