fd0876c122cca472f4e98a2fff64b68b296bc3e6
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2023-12-30 22:11:36 -0500

    Create nonCombatUnits.ts

---
+++ src/asoiaf/boltons/nonCombatUnits.ts
@@ -0,0 +1,18 @@
+import { CardData, CardTypes } from '../types';
+
+const cardData: CardData = {
+  'name': {
+    name: 'name',
+    type: CardTypes.NonCombatUnit,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc-back.png',
+  },
+  'name': {
+    name: 'name',
+    type: CardTypes.NonCombatUnit,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc-back.png',
+  },
+};
+
+export default cardData;
