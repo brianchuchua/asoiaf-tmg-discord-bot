1b2fe2160331fbca5f9f1c8aca5a592e38fabd4d
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2023-12-30 22:59:16 -0500

    Update attachments.ts

--- src/asoiaf/boltons/attachments.ts
+++ src/asoiaf/boltons/attachments.ts
@@ -1,6 +1,24 @@
 import { CardData, CardTypes } from '../types';
 
 const cardData: CardData = {
+  'Ben Bones - Kennelmaster': {
+    name: 'Ben Bones - Kennelmaster',
+    type: CardTypes.Attachment,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ben-bones-k.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ben-bones-k-back.png',
+  },
+  'Bolton Flayer': {
+    name: 'Bolton Flayer',
+    type: CardTypes.Attachment,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-bolton-flayer.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/n-bolton-flayer-back.png',
+  },
+  'Dreadfort Captain': {
+    name: 'Dreadfort Captain',
+    type: CardTypes.Attachment,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-dreadfort-captain.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/n-dreadfort-captain-back.png',
+  },
   'Ramsay Snow - Spiteful Heir': {
     name: 'Ramsay Snow - Spiteful Heir',
     type: CardTypes.Attachment,
@@ -8,11 +26,19 @@ const cardData: CardData = {
     imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ramsay-snow-sh-back.png',
     isCommander: true,
   },
-  'Name': {
-    name: 'Name',
+  'Roose Bolton - Lord of the Dreadfort': {
+    name: 'Roose Bolton - Lord of the Dreadfort',
     type: CardTypes.Attachment,
-    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc.png',
-    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc-back.png',
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-roose-bolton-lotd.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-roose-bolton-lotd-back.png',
+    isCommander: true,
+  },
+  'Steelshanks Walton - Captain of the Guard': {
+    name: 'Steelshanks Walton - Captain of the Guard',
+    type: CardTypes.Attachment,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-steelshanks-walton-cotg.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-steelshanks-walton-cotg-back.png',
+    isCommander: true,
   },
 };
 
