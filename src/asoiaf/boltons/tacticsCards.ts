3fe2cb1ca55db34f2197b3ffbac5292cd077dcdc
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2023-12-30 22:13:39 -0500

    Create tacticsCards.ts

---
+++ src/asoiaf/boltons/tacticsCards.ts
@@ -0,0 +1,54 @@
+import { CardData, CardTypes } from '../types';
+
+const cardData: CardData = {
+  'Baratheon Justice - Baratheon': {
+    name: 'Baratheon Justice - Baratheon',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t1.png',
+    imageUrlBack: '',
+  },
+  "Stag's Wit - Baratheon": {
+    name: "Stag's Wit - Baratheon",
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t2.png',
+    imageUrlBack: '',
+  },
+  'Ours is the Fury! - Baratheon': {
+    name: 'Ours is the Fury! - Baratheon',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t3.png',
+    imageUrlBack: '',
+  },
+  'Sustained Assault - Baratheon': {
+    name: 'Sustained Assault - Baratheon',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t4.png',
+    imageUrlBack: '',
+  },
+  'Baratheon Conviction - Baratheon': {
+    name: 'Baratheon Conviction - Baratheon',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t5.png',
+    imageUrlBack: '',
+  },
+  'Final Strike - Baratheon': {
+    name: 'Final Strike - Baratheon',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t6.png',
+    imageUrlBack: '',
+  },
+  'Oath of Duty - Baratheon': {
+    name: 'Oath of Duty - Baratheon',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t7.png',
+    imageUrlBack: '',
+  },
+  'Wealth and Charisma - Renly Baratheon (The Charismatic Heir)': {
+    name: 'Wealth and Charisma - Renly Baratheon (The Charismatic Heir)',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-renly-baratheon-tch-t1.png',
+    imageUrlBack: '',
+  },
+};
+
+export default cardData;
