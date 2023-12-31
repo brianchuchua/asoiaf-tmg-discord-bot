96e2684b14ec31a93ba139b72c22ea5e017d3de1
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2023-12-30 22:06:18 -0500

    Create attachments.ts

---
+++ src/asoiaf/boltons/attachments.ts
@@ -0,0 +1,19 @@
+import { CardData, CardTypes } from '../types';
+
+const cardData: CardData = {
+  'Name': {
+    name: 'Name',
+    type: CardTypes.Attachment,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc-back.png',
+    isCommander: true,
+  },
+  'Name': {
+    name: 'Name',
+    type: CardTypes.Attachment,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc-back.png',
+  },
+};
+
+export default cardData;
