92bfccc760baeeb45e7663bf12943fc5b928e8d4
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2023-12-30 22:08:44 -0500

    Create combatUnits.ts

---
+++ src/asoiaf/boltons/combatUnits.ts
@@ -0,0 +1,18 @@
+import { CardData, CardTypes } from '../types';
+
+const cardData: CardData = {
+  'unit name': {
+    name: 'unit name',
+    type: CardTypes.CombatUnit,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-unit.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-unit-back.png',
+  },
+  'unit name': {
+    name: 'unit name',
+    type: CardTypes.CombatUnit,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-unit.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-unit-back.png',
+  },
+};
+
+export default cardData;
