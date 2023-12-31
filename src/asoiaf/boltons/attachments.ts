b1c4681cea86f2434fd93ac63fc7de460e09d4c4
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2023-12-30 22:39:12 -0500

    Update attachments.ts

--- src/asoiaf/boltons/attachments.ts
+++ src/asoiaf/boltons/attachments.ts
@@ -1,11 +1,11 @@
 import { CardData, CardTypes } from '../types';
 
 const cardData: CardData = {
-  'Name': {
-    name: 'Name',
+  'Ramsay Snow - Spiteful Heir': {
+    name: 'Ramsay Snow - Spiteful Heir',
     type: CardTypes.Attachment,
-    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc.png',
-    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-name-abc-back.png',
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ramsay-snow-sh.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ramsay-snow-sh-back.png',
     isCommander: true,
   },
   'Name': {
