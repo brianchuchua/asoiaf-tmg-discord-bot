ba198bd29f079d540a2089754b932f8a3bf5d159
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2023-12-30 23:16:23 -0500

    Update tacticsCards.ts

--- src/asoiaf/boltons/tacticsCards.ts
+++ src/asoiaf/boltons/tacticsCards.ts
@@ -61,6 +61,42 @@ const cardData: CardData = {
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ramsay-snow-sh-t3.png',
     imageUrlBack: '',
   },
+  'Whispered Threats - Roose Bolton (Lord of the Dreadfort)': {
+    name: 'Whispered Threats - Roose Bolton (Lord of the Dreadfort)',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-roose-bolton-lotd-t1.png',
+    imageUrlBack: '',
+  },
+  'Seeing Their Flaws - Roose Bolton (Lord of the Dreadfort)': {
+    name: 'Seeing Their Flaws - Roose Bolton (Lord of the Dreadfort)',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-roose-bolton-lotd-t2.png',
+    imageUrlBack: '',
+  },
+  'Dreadfort Secrets - Roose Bolton (Lord of the Dreadfort)': {
+    name: 'Dreadfort Secrets - Roose Bolton (Lord of the Dreadfort)',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-roose-bolton-lotd-t3.png',
+    imageUrlBack: '',
+  },
+  'Price of Fear - Steelshanks Walton (Captain of the Guard)': {
+    name: 'Price of Fear - Steelshanks Walton (Captain of the Guard)',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-steelshanks-walton-cotg-t1.png',
+    imageUrlBack: '',
+  },
+  'Rush of Aggression - Steelshanks Walton (Captain of the Guard)': {
+    name: 'Rush of Aggression - Steelshanks Walton (Captain of the Guard)',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-steelshanks-walton-cotg-t2.png',
+    imageUrlBack: '',
+  },
+  'Taunt - Steelshanks Walton (Captain of the Guard)': {
+    name: 'Taunt - Steelshanks Walton (Captain of the Guard)',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-steelshanks-walton-cotg-t3.png',
+    imageUrlBack: '',
+  },
 };
 
 export default cardData;
