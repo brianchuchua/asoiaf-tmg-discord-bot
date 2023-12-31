9efe7412bf59b817fe7a1b6f51ee1449fb8cc90e
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2023-12-30 22:25:59 -0500

    Update tacticsCards.ts
    Add base Bolton cards & Ramsay Snow

--- src/asoiaf/boltons/tacticsCards.ts
+++ src/asoiaf/boltons/tacticsCards.ts
@@ -1,52 +1,64 @@
 import { CardData, CardTypes } from '../types';
 
 const cardData: CardData = {
-  'Baratheon Justice - Baratheon': {
-    name: 'Baratheon Justice - Baratheon',
+  'Remorseless Examples - Bolton': {
+    name: 'Remorseless Examples - Bolton',
     type: CardTypes.TacticsCard,
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t1.png',
     imageUrlBack: '',
   },
-  "Stag's Wit - Baratheon": {
-    name: "Stag's Wit - Baratheon",
+  'Harsh Punishments - Bolton': {
+    name: 'Harsh Punishments - Bolton',
     type: CardTypes.TacticsCard,
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t2.png',
     imageUrlBack: '',
   },
-  'Ours is the Fury! - Baratheon': {
-    name: 'Ours is the Fury! - Baratheon',
+  'Our Blades Are Sharp - Bolton': {
+    name: 'Our Blades Are Sharp - Bolton',
     type: CardTypes.TacticsCard,
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t3.png',
     imageUrlBack: '',
   },
-  'Sustained Assault - Baratheon': {
-    name: 'Sustained Assault - Baratheon',
+  'Sadistic Games - Bolton': {
+    name: 'Sadistic Games - Bolton',
     type: CardTypes.TacticsCard,
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t4.png',
     imageUrlBack: '',
   },
-  'Baratheon Conviction - Baratheon': {
-    name: 'Baratheon Conviction - Baratheon',
+  'Cruel Methods - Bolton': {
+    name: 'Cruel Methods - Bolton',
     type: CardTypes.TacticsCard,
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t5.png',
     imageUrlBack: '',
   },
-  'Final Strike - Baratheon': {
-    name: 'Final Strike - Baratheon',
+  'A Flayed Man Has No Secrets - Bolton': {
+    name: 'A Flayed Man Has No Secrets - Bolton',
     type: CardTypes.TacticsCard,
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t6.png',
     imageUrlBack: '',
   },
-  'Oath of Duty - Baratheon': {
-    name: 'Oath of Duty - Baratheon',
+  'Fear Keeps A Man Alive - Bolton': {
+    name: 'Fear Keeps A Man Alive - Bolton',
     type: CardTypes.TacticsCard,
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-t7.png',
     imageUrlBack: '',
   },
-  'Wealth and Charisma - Renly Baratheon (The Charismatic Heir)': {
-    name: 'Wealth and Charisma - Renly Baratheon (The Charismatic Heir)',
+  'Skin Collection - Ramsay Snow (Spiteful Heir)': {
+    name: 'Skin Collection - Ramsay Snow (Spiteful Heir)',
     type: CardTypes.TacticsCard,
-    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-renly-baratheon-tch-t1.png',
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ramsay-snow-sh-t1.png',
+    imageUrlBack: '',
+  },
+  'Opportunist - Ramsay Snow (Spiteful Heir)': {
+    name: 'Opportunist - Ramsay Snow (Spiteful Heir)',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ramsay-snow-sh-t2.png',
+    imageUrlBack: '',
+  },
+  'Spoils In Flesh - Ramsay Snow (Spiteful Heir)': {
+    name: 'Spoils In Flesh - Ramsay Snow (Spiteful Heir)',
+    type: CardTypes.TacticsCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ramsay-snow-sh-t3.png',
     imageUrlBack: '',
   },
 };
