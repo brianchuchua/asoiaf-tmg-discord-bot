7326033eb191c5b18e7b83aeac2b1aa2d3ac69ba
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2023-12-31 22:46:38 -0500

    Reformat to match Martell index

--- src/asoiaf/greyjoys/index.ts
+++ src/asoiaf/greyjoys/index.ts
@@ -1,14 +1,15 @@
 import Attachments from './attachments';
 import CombatUnits from './combatUnits';
+import InformationCards from './informationCards';
 import NonCombatUnits from './nonCombatUnits';
 import OpponentAttachments from './opponentAttachments';
 import TacticsCards from './tacticsCards';
-import InformationCards from './informationCards';
 
-export default 
-{ ...CombatUnits, 
-  ...Attachments, 
-  ...NonCombatUnits, 
-  ...OpponentAttachments, 
+export default {
+  ...CombatUnits,
+  ...Attachments,
+  ...NonCombatUnits,
+  ...OpponentAttachments,
   ...TacticsCards,
-  ...InformationCards};
+  ...InformationCards,
+};
