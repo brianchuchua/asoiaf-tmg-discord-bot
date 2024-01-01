e23a92d0b8d26119150aaba232b7683d9153176b
Brian Chuchua <bchuchua@gmail.com>
2024-01-01 12:16:19 -0800

    fix: add a missing comma

--- src/asoiaf/boltons/nonCombatUnits.ts
+++ src/asoiaf/boltons/nonCombatUnits.ts
@@ -12,7 +12,7 @@ const cardData: CardData = {
     type: CardTypes.NonCombatUnit,
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ramsay-snow-rh.png',
     imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ramsay-snow-rh-back.png',
-  }
+  },
   'Roose Bolton - Calculating and Cruel': {
     name: 'Roose Bolton - Calculating and Cruel',
     type: CardTypes.NonCombatUnit,
