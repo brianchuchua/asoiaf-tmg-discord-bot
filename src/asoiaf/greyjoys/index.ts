d786513a7c485a7d06f4062efa5f3a6b27e46721
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2023-12-31 22:45:39 -0500

    Update index.ts

--- src/asoiaf/greyjoys/index.ts
+++ src/asoiaf/greyjoys/index.ts
@@ -1,8 +1,14 @@
 import Attachments from './attachments';
 import CombatUnits from './combatUnits';
-import InformationCards from './informationCards';
 import NonCombatUnits from './nonCombatUnits';
 import OpponentAttachments from './opponentAttachments';
 import TacticsCards from './tacticsCards';
+import InformationCards from './informationCards';
 
-export default { ...CombatUnits, ...Attachments, ...NonCombatUnits, ...OpponentAttachments, ...TacticsCards };
+export default 
+{ ...CombatUnits, 
+  ...Attachments, 
+  ...NonCombatUnits, 
+  ...OpponentAttachments, 
+  ...TacticsCards,
+  ...InformationCards};
