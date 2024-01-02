1fb580cb72abc8068563c08bb368ccae79c5673a
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2024-01-01 21:33:14 -0500

    Add Bolton tag to avoid duplicate of Roose NCU

--- src/asoiaf/boltons/nonCombatUnits.ts
+++ src/asoiaf/boltons/nonCombatUnits.ts
@@ -13,8 +13,8 @@ const cardData: CardData = {
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ramsay-snow-rh.png',
     imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-ramsay-snow-rh-back.png',
   },
-  'Roose Bolton - Calculating and Cruel': {
-    name: 'Roose Bolton - Calculating and Cruel',
+  'Roose Bolton - Calculating and Cruel (Bolton)': {
+    name: 'Roose Bolton - Calculating and Cruel (Bolton)',
     type: CardTypes.NonCombatUnit,
     imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-roose-bolton-cac.png',
     imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-roose-bolton-cac-back.png',
