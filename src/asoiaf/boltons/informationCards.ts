ef6d86d07449fd42881dc086b0cb4562d9efbaab
nrWillbanks <72263329+nrWillbanks@users.noreply.github.com>
2025-06-18 19:07:05 -0400

    Update informationCards.ts
    Added Dreadfort Prisoners

--- src/asoiaf/boltons/informationCards.ts
+++ src/asoiaf/boltons/informationCards.ts
@@ -1,6 +1,12 @@
 import { CardData, CardTypes } from '../types';
 
 const cardData: CardData = {
+  'Dreadfort Prisoners': {
+    name: 'Dreadfort Prisoners',
+    type: CardTypes.InformationCard,
+    imageUrl: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-dreadfort-prisoners.png',
+    imageUrlBack: 'https://asoiaf-tmg-discord-bot.s3.amazonaws.com/images/hbo-dreadfort-prisoners-back.png',
+  },
   'Inner Circle': {
     name: 'Inner Circle',
     type: CardTypes.InformationCard,
